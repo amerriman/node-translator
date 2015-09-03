01ba0d825d69f2b98b8c3536fe139916feb95d25
keithhopkins <knhopkins412@gmail.com>
2015-09-03 10:30:35 -0600

    adds translate requests

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,5 +1,19 @@
 // add scripts
 
+
 $(document).on('ready', function() {
-  console.log('sanity check!');
+  // $.ajax({
+  //   method: 'post',
+  //   url: 'http://api.microsofttranslator.com&client_id=TRexg1128?client_secret=SZCUOEd0phCVkILVyY5ViZsBTzUpctF6GLvPgNpfON4=?grant_type=client_credentials',
+  //   type: 'jsonp'
+  // })
+  // .done(function(data){
+  //   console.log(data);
+  // })
+  // .fail(function(err){
+  //   console.log(err);
+  // });
+
+
+  
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,16 @@
+var keys = require('keys');
 var express = require('express');
 var router = express.Router();
+var bt = require('../../node_modules/bing-translate/lib/bing-translate.js').init({
+  client_id: keys.client_id,
+  client_secret: keys.client_secret
+});
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+  bt.translate('This hotel is located close to the centre of Paris.', 'en', 'ro', function(err, res){
+    console.log(err, res);
+  });
 });
 
 module.exports = router;
