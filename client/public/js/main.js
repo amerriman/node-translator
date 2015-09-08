ddbcdb749384c095ddfb68b494e63b23761702f9
Robert Hajek <hajek.rob@gmail.com>
2015-09-08 10:06:49 -0600

    saved state, working front end

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,24 +1,3 @@
-// add scripts
-
-
-$('.start-quiz').on('click', function() {
-  $('#translated-word').hide();
-  $('.flashcard').show();
-  $('#answer-form').show();
-  $('#results').hide();
-  $('#translate').hide();
-  $('.translator').show();
-});
-
-$('.translator').on('click', function() {
-  $('#translated-word').show();
-  $('.flashcard').hide();
-  $('#answer-form').hide();
-  $('#results').show();
-  $('#translate').show();
-  $('.translator').hide();
-});
-
 $(document).on('ready', function() {
   $('.translator').hide();
   $('.flashcard').hide();
@@ -26,37 +5,6 @@ $(document).on('ready', function() {
   $('.flashcard').on('click', function() {
     $('.flashcard').toggleClass('flipped');
   });
-
-  $('.start-quiz').on('click', function() {
-    $.ajax({
-      method: 'post',
-      url: '/quiz',
-      data: {
-        language: $('#toLanguage').val()
-      }
-    })
-    .done(function(data){
-      currentList = data;
-    });
-  });
-
-
-  $('#quiz-answer').on('click', function() {
-    console.log('test');
-    var res = result();
-    $.ajax({
-      method: 'post',
-      url: '/answer',
-      data: {
-        word: currentWord,
-        correct: res
-      }
-    }).done(function() {
-      displayAnswer();
-      console.log('success');
-    });
-  });
-
 
   $('form').on('submit', function(e){
     e.preventDefault();
@@ -73,4 +21,61 @@ $(document).on('ready', function() {
       }
     });
   });
+});
+
+// add scripts
+$('.start-quiz').on('click', function() {
+  $('#translated-word').hide();
+  $('.flashcard').show();
+  $('#answer-form').show();
+  $('#results').hide();
+  $('#translate').hide();
+  $('.translator').show();
+  $('.start-quiz').hide();
+  $('#next-question').hide();
+
+  $.ajax({
+    method: 'post',
+    url: '/quiz',
+    data: {
+      fromLanguage: $('#fromLanguage').val(),
+      toLanguage: $('#toLanguage').val()
+    }
+  })
+  .done(function(data){
+    currentList = data;
+    nextWord();
+  });
+});
+
+$('.translator').on('click', function() {
+  $('#translated-word').show();
+  $('.flashcard').hide();
+  $('#answer-form').hide();
+  $('#results').show();
+  $('#translate').show();
+  $('.translator').hide();
+  $('.start-quiz').show();
+});
+
+$('#quiz-answer').on('click', function() {
+  console.log('test');
+  var res = result();
+  $.ajax({
+    method: 'post',
+    url: '/answer',
+    data: {
+      word: currentWord,
+      correct: res
+    }
+  }).done(function() {
+    displayAnswer();
+    console.log('success');
+  });
+  $('#quiz-answer').hide();
+  $('#next-question').show();
+});
+
+$('#next-question').on('click', function() {
+  // nextquestion();
 });
